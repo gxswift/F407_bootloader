a459fbcc63ef152b84c0acbcc15e683fd1394a0e
GX <853129649@qq.com>
2019-04-12 14:08:24 +0800

    restore RTC init error

--- Src/main.c
+++ Src/main.c
@@ -35,7 +35,9 @@ void MX_RTC_Init(void)
   hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
   if (HAL_RTC_Init(&hrtc) != HAL_OK)
   {
+		printf("Bootloader RTC error\r\n");
   }
+	//__HAL_RCC_CLEAR_RESET_FLAGS();
 }
 
 void HAL_RTC_MspInit(RTC_HandleTypeDef* rtcHandle)
@@ -449,9 +451,7 @@ static void LCD_SetCursor(uint16_t usX,uint16_t usY)
 static __inline void LCD_FillColor ( uint32_t ulAmout_Point, uint16_t usColor )
 {
 	uint32_t i = 0;	
-	
 	LCD_WRITE_CMD ( 0x2C );	
-	
 	for ( i = 0; i < ulAmout_Point; i ++ )
 		LCD_WRITE_DATA ( usColor );	
 }
@@ -597,6 +597,43 @@ void SystemClock_Config(void)
 
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
+ 
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;       
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;   
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+
+  HAL_RCC_EnableCSS();                      
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+  PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+  }
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000); 
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+/*
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
  
   __HAL_RCC_PWR_CLK_ENABLE();
 
@@ -625,7 +662,7 @@ void SystemClock_Config(void)
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000); 
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-}
+*/
 //-------------------------------------------------------------------------------------------------------------------------
 #define FLASH_APP1_ADDR		0x08010000
 
@@ -905,6 +942,7 @@ void USB_IAP()
 		#endif
 		HAL_HCD_MspDeInit(&hhcd_USB_OTG_FS);
 		HAL_UART_MspDeInit(&huart1);
+			__HAL_RCC_RTC_DISABLE();
 		//NVIC_SystemReset();
 		iap_load_app(FLASH_APP1_ADDR);
 	}else 
@@ -952,6 +990,7 @@ int main(void)
 	
 	if ((HAL_GPIO_ReadPin(GPIOE,GPIO_PIN_0) == 1 ) && ((*(volatile uint32_t *)0x080E0000!=0XB9F9D0C2)))
 	{
+		printf("%X\t",*(volatile uint32_t *)0x080E0000);
 			printf("Jump to APP\r\n");
 			iap_load_app(FLASH_APP1_ADDR);
 	}
